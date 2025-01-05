6fd636757f622cae71a6107676e937da15df9d94
RenatoSantos7 <renatodisantos@outlook.com>
2025-01-04 22:44:48 -0300

    added fish themes

---
+++ .config/fish/conf.d/colors.fish
@@ -0,0 +1,54 @@
+set -l rosewater f5e0dc
+set -l flamingo f2cdcd
+set -l pink f5c2e7
+set -l mauve cba6f7
+set -l red f38ba8
+set -l maroon eba0ac
+set -l peach fab387
+set -l yellow f9e2af
+set -l green a6e3a1
+set -l teal 94e2d5
+set -l sky 89dceb
+set -l sapphire 74c7ec
+set -l blue 89b4fa
+set -l lavender b4befe
+set -l text cdd6f4
+set -l base 1e1e2e
+set -l mantle 181825
+set -l crust 11111b
+set -l subtext_1 bac2de
+set -l subtext_0 a6adc8
+set -l overlay_2 9399b2
+set -l overlay_1 7f849c
+set -l overlay_0 6c7086
+set -l surface_2 585b70
+set -l surface_1 45475a
+set -l surface_0 313244
+
+
+set -g fish_color_normal $text
+set -g fish_color_command $blue --bold
+set -g fish_color_param 74c7ec 
+set -g fish_color_keyword f38ba8
+set -g fish_color_quote f9e2af
+set -g fish_color_redirection f5c2e7
+set -g fish_color_end fab387
+set -g fish_color_comment $subtext_0
+set -g fish_color_error $maroon --bold
+set -g fish_color_gray $overlay_0
+set -g fish_color_selection --background=313244
+set -g fish_color_search_match --background=313244
+set -g fish_color_option $rosewater
+set -g fish_color_operator $lavender
+set -g fish_color_escape eba0ac
+set -g fish_color_autosuggestion 6c7086
+set -g fish_color_cancel $pink --bold
+set -g fish_color_cwd f9e2af
+set -g fish_color_user 94e2d5
+set -g fish_color_host 89b4fa
+set -g fish_color_host_remote a6e3a1
+set -g fish_color_status f38ba8
+set -g fish_pager_color_progress 6c7086
+set -g fish_pager_color_prefix f5c2e7
+set -g fish_pager_color_completion cdd6f4
+set -g fish_pager_color_description 6c7086
